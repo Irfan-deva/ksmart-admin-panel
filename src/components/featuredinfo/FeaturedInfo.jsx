196a29cbab80a5893514a1d37ad7b10b857710b2
irfan-deva <irfandeva@gmail.com>
2023-10-29 10:36:12 +0530

    improved design a bit

--- src/components/featuredinfo/FeaturedInfo.jsx
+++ src/components/featuredinfo/FeaturedInfo.jsx
@@ -1,25 +1,60 @@
 import React from 'react'
 import './FeaturedInfo.css'
+import { Widgets, Category, ShoppingCart, People } from '@mui/icons-material'
+
+
 function FeaturedInfo() {
   return (
     <div className='featured'>
+
       <div className="featuredItem">
-        <span className="featuredTitle">Total Products</span>
-        <div className="totalContainer">
-          <span className="total">255</span>
+        <div className="featuredIconContainer">
+          <Widgets className='featuredIcon' />
+        </div>
+        <div className="featuredInfo">
+          <span className="featuredTitle">Total Products</span>
+          <div className="totalContainer">
+            <span className="total">255</span>
+          </div>
         </div>
       </div>
+
       <div className="featuredItem">
-        <span className="featuredTitle">Total Categories</span>
-        <div className="totalContainer">
-          <span className="total">5</span>
+        <div className="featuredIconContainer">
+          <Category className='featuredIcon' />
         </div>
-      </div> <div className="featuredItem">
-        <span className="featuredTitle">Total Orders</span>
-        <div className="totalContainer">
-          <span className="total">100</span>
+        <div className="featuredInfo">
+          <span className="featuredTitle">Total Categories</span>
+          <div className="totalContainer">
+            <span className="total">5</span>
+          </div>
         </div>
       </div>
+
+      <div className="featuredItem">
+        <div className="featuredIconContainer">
+          <ShoppingCart className='featuredIcon' />
+        </div>
+        <div className="featuredInfo">
+          <span className="featuredTitle">Total Orders</span>
+          <div className="totalContainer">
+            <span className="total">100</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <div className="featuredIconContainer">
+          <People className='featuredIcon' />
+        </div>
+        <div className="featuredInfo">
+          <span className="featuredTitle">Total Users</span>
+          <div className="totalContainer">
+            <span className="total">10k</span>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
